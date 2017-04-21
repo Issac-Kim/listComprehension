a720c9c05461418801bb70b0397ef59278c531f1
Issac Kim <ikim1@stuy.edu>
2017-04-21 14:26:14 -0400

    finish pass integrity

--- password.py
+++ password.py
@@ -14,3 +14,22 @@ def passCheck(password):
 #Second
 
 def passIntegrity(password):
+    password = list(password)
+    if(passCheck(password)):
+        score = 1
+        if len(password) == 3:
+            return 1
+        alpha = (len([x for x in password if x.isalpha()]) - 1) / 2
+        num = len([x for x in password if x.isdigit()])
+        spec = len([x for x in password if not x.isalnum()])
+        if alpha > 3:
+            alpha = 3
+        if num > 3:
+            num = 3
+        if spec > 3:
+            spec = 3
+        return score + alpha + num + spec
+    else:
+        return 0
+
+
